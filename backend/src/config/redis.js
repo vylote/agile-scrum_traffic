981d4b9f8188fc6e11dfa6c56d6f2f6a121c4945
VyLoTe <antran064969@gmail.com>
2026-05-09 20:11:08 +0700

    fix: redis init

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,5 +1,3 @@
 module.exports = {
-    host: process.env.REDIS_HOST || '127.0.0.1',
-    port: process.env.REDIS_PORT || 6379,
-    // password: process.env.REDIS_PASSWORD || '',
+    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
 };
